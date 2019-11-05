f86a7880f082271af6b1f9edbfe2b100ffaffea4
Tony Clonier <clonier.tony@hotmail.fr>
2019-11-05 22:28:06 +0100

    Formated main.go

--- main.go
+++ main.go
@@ -12,20 +12,35 @@ func main() {
 	env := &handlers.Env{}
 	var err error
 
+	env.Templates = template.Must(template.ParseGlob("./templates/*"))
 	env.DB, err = models.InitDB("db/test.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer env.DB.Close()
 
-	env.Templates = template.Must(template.ParseGlob("./templates/*"))
-
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.Handle("/fill_db", handlers.Handler{Env: env, HandlerFunc: handlers.FillDBHandler})
-	http.Handle("/create_user", handlers.Handler{Env: env, HandlerFunc: handlers.CreateUserHandler})
-	http.Handle("/users", handlers.Handler{Env: env, HandlerFunc: handlers.UsersHandler})
-	http.Handle("/user", handlers.Handler{Env: env, HandlerFunc: handlers.UserHandler})
+
+	http.Handle("/fill_db", handlers.Handler{
+		Env:         env,
+		HandlerFunc: handlers.FillDBHandler,
+	})
+
+	http.Handle("/create_user", handlers.Handler{
+		Env:         env,
+		HandlerFunc: handlers.CreateUserHandler,
+	})
+
+	http.Handle("/users", handlers.Handler{
+		Env:         env,
+		HandlerFunc: handlers.UsersHandler,
+	})
+
+	http.Handle("/user", handlers.Handler{
+		Env:         env,
+		HandlerFunc: handlers.UserHandler,
+	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
